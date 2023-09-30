48a19ad6b5ee77fe1002798a7e3279fbc54f4067
frostnight <frostnightsong@gmail.com>
2023-09-30 22:52:26 +0900

    회원가입

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,19 @@
+package hello.login.domain.member;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private Long id;
+
+	@NotEmpty
+	private String loginId;	// 로그인 ID
+	@NotEmpty
+	private String name;
+	@NotEmpty
+	private String password;
+}
