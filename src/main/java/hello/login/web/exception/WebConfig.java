8332061e6e94e4d3267113d85eee5a56db0e3ac2
frostnight <frostnightsong@gmail.com>
2024-02-12 14:39:58 +0900

    서블릿 예외 처리 인터셉터

--- src/main/java/hello/login/web/exception/WebConfig.java
+++ src/main/java/hello/login/web/exception/WebConfig.java
@@ -7,14 +7,24 @@ import javax.servlet.FilterRegistration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.interceptor.LogInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+	}
+
+	// @Bean
 	public FilterRegistrationBean logFilter(){
 		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
 		filterFilterRegistrationBean.setFilter(new LogFilter());
