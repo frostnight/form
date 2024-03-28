3dc986bd6c220ce4776ef6fcbbfc763f0308b7bc
frostnight <frostnightsong@gmail.com>
2024-03-29 02:19:28 +0900

    ExceptionHandler

--- src/main/java/hello/login/web/exception/api/ApiExceptionController.java
+++ src/main/java/hello/login/web/exception/api/ApiExceptionController.java
@@ -8,15 +8,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class ApiExceptionController {
 
-    @GetMapping("/api/members/{id}")
+    @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if(id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
@@ -31,17 +33,17 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello" + id);
     }
 
-    @GetMapping("/api/response-status-ex1")
+    @GetMapping("/response-status-ex1")
     public String responseStatusEx1() {
         throw new BadRequestException();
     }
 
-    @GetMapping("/api/response-status-ex2")
+    @GetMapping("/response-status-ex2")
     public String responseStatusEx2() {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
-    @GetMapping("/api/default-handler-ex")
+    @GetMapping("/default-handler-ex")
     public String defaultException(@RequestParam Integer data) {
         return "ok";
     }
